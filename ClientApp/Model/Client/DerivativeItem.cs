d138512aa73ca508c086cdf970dc13b086f465ea
Rob Little <rlittle@thetasoft.com>
2024-04-18 17:12:37 -0700

    some comments for clarity and remove dead code

--- ClientApp/Model/Client/DerivativeItem.cs
+++ ClientApp/Model/Client/DerivativeItem.cs
@@ -7,6 +7,18 @@ using Thetacat.Util;
 
 namespace Thetacat.Model.Client;
 
+/*----------------------------------------------------------------------------
+    %%Class: DerivativeItem
+    %%Qualified: Thetacat.Model.Client.DerivativeItem
+
+    This holds a single derived item. If PendingBitmap is set, then this item
+    has not been commited to disk yet. When we complete the save, then the
+    path will be set and PendingBitmap will be set to null.
+
+    Saves will be queued up using QueueSaveResampledImage and
+    QueueSaveReformatImage. Both of these happen when we go to actually
+    do the work of loading the image (also on a background thread)
+----------------------------------------------------------------------------*/
 public class DerivativeItem
 {
     private PathSegment? m_pathSegment;
@@ -30,6 +42,12 @@ public class DerivativeItem
     public string TransformationsKey { get; set; }
     public bool IsSaveQueued => PendingBitmap != null;
 
+    /*----------------------------------------------------------------------------
+        %%Function: DerivativeItem
+        %%Qualified: Thetacat.Model.Client.DerivativeItem.DerivativeItem
+
+        Create a pending derivative item
+    ----------------------------------------------------------------------------*/
     public DerivativeItem(Guid mediaId, string mimeType, double scaleFactor, string transformationsKey, BitmapSource pendingBitmap)
     {
         MediaId = mediaId;
@@ -40,16 +58,12 @@ public class DerivativeItem
         TransformationsKey = transformationsKey;
     }
 
-    public DerivativeItem(Guid mediaId, string mimeType, double scaleFactor, string transformationsKey, PathSegment path)
-    {
-        MediaId = mediaId;
-        MimeType = mimeType;
-        ScaleFactor = scaleFactor;
-        m_pathSegment = path;
-        Pending = true;
-        TransformationsKey = transformationsKey;
-    }
+    /*----------------------------------------------------------------------------
+        %%Function: DerivativeItem
+        %%Qualified: Thetacat.Model.Client.DerivativeItem.DerivativeItem
 
+        Create a new item from a derivative from the database
+    ----------------------------------------------------------------------------*/
     public DerivativeItem(DerivativeDbItem dbItem)
     {
         MediaId = dbItem.MediaId;
